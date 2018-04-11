46cbafb6a7df5c84c94296590e3120111313220b
ranadewa <ranadewa.thisara@gmail.com>
2018-04-11 17:32:56 +0700

    GTEST Project Template created

---
+++ ProjectTemplate/test.cpp
@@ -0,0 +1,16 @@
+#include "Main.cpp"
+#include <gtest/gtest.h>
+
+/*
+TEST(InitalValueTest, checkRandNum) { 
+
+	interval_map<int,char> testMap('Z');
+
+	int count = 1000;
+
+	for (int i = 0; i < count; ++i)
+	{
+		ASSERT_EQ('Z', testMap[rand()]);
+	}
+}
+*/
