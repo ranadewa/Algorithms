ff64af4900d44a319053004b27f59e0b7efbac45
ranadewa <ranadewa.thisara@gmail.com>
2018-04-13 15:21:45 +0700

    Min tickets done

--- CrackingTheCodingTest/BuyMinimumTickets/Main.cpp
+++ CrackingTheCodingTest/BuyMinimumTickets/Main.cpp
@@ -1,4 +1,110 @@
 #include <iostream>
+#include <vector> 
+#include <algorithm>
 
 using namespace std;
 
+
+
+
+int 	solve(int* A, int size, int index, int max) {
+    
+    	cout << endl;
+    	cout << "size " << size <<" index " << index << " Max " << max;
+    	int result = 0;
+
+        if(index >= size) {
+        	cout << " returns " << 0 << endl;
+            return 0;
+        }
+
+        if(A[index] <= max) {
+
+        	result = solve(A, size, index+1, max);
+        	cout << " returns " << result << endl;
+            return result;
+        } else {
+
+        		cout << " Counting Left ";
+				int left = solve(A, size, index+1, 0)+2;
+				cout << left << endl;
+
+				cout << " Counting Right " ;
+				int right = solve(A, size, index+1, A[index]+6)+7;
+				cout << right << endl;
+
+
+        	result = (min( right , left));
+
+        	cout << " returns " << result << endl;
+
+            return result;
+        }
+        
+}
+
+int solutionRecursive(std::vector <int> v)
+{
+
+	if (v.size() <= 3) return v.size() * 2;
+	if (v.size() >= 23) return 25;
+	
+	int* arr = new int[v.size()];
+
+	std::copy(v.begin(), v.end(), arr);
+//	cout << arr[0] << endl;
+	
+	cout << "Counting Left ";
+	int left = solve(arr, v.size(), 1, 0)+2;
+	cout <<" In Main Left " << left << endl;
+
+	cout << "Counting Right " ;
+	int right = solve(arr, v.size(), 1, arr[0]+6)+7 ;
+	cout << "In Main right " << right << endl;
+
+	int ans = min(right, left );
+
+    return min(ans, 25);
+
+}
+
+
+int solution(vector<int> days)
+{
+
+	if (days.size() ==0 )
+	{
+		return 0;
+	}
+
+	const int ONEDAY_TICKET = 2;
+	const int ONEWEEK_TICKET = 7;
+	const int ONEMONTH_TICKET = 25;
+
+	int WeekBucket = 0;
+
+	int firstElement = days[0];
+	int total = 0;
+
+	for(auto day : days)
+	{
+		if ( day - firstElement <= 6) // in the same bucket 
+		{
+			WeekBucket++;
+		}
+		else  // Out of the assigned bucket
+		{
+			total += (WeekBucket <= 3) ? WeekBucket*ONEDAY_TICKET : ONEWEEK_TICKET;
+			firstElement = day;
+
+			WeekBucket = 1;
+		}
+	}
+
+	total += (WeekBucket <= 3) ? WeekBucket*ONEDAY_TICKET : ONEWEEK_TICKET;
+
+
+	total = (total <= 24) ? total : ONEMONTH_TICKET;
+
+	return total;
+}
--- CrackingTheCodingTest/BuyMinimumTickets/test.cpp
+++ CrackingTheCodingTest/BuyMinimumTickets/test.cpp
@@ -7,6 +7,59 @@ int main(int argc, char **argv) {
 }
 
 
+// TEST(testsolution, lessThan3) { 
+
+// 	ASSERT_EQ(6, solution({1, 2, 6}));
+// 	ASSERT_EQ(4, solution({1, 25}));
+// 	ASSERT_EQ(2, solution({1}));
+// 	ASSERT_EQ(0, solution({}));
+// }
+
+// TEST(testsolution, greaterThan24) { 
+
+// 	ASSERT_EQ(25, solution({1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}));
+// 	ASSERT_EQ(25, solution({4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,28, 29,30}));
+// }
+
+
+
+// TEST(testsolution, HKtest) { 
+
+// 	ASSERT_EQ(11, solution({1, 2, 4, 5,7,29, 30}));
+// 	ASSERT_EQ(7, solution({5,6,7,8,9,10}));
+
+// 	ASSERT_EQ(14, solution({5,6,7,8,9,10, 12,13,14,15}));
+
+// 	ASSERT_EQ(16, solution({5,6,7,8,9,10, 21,27,28,29,30}));
+// }
+
+
+// TEST(solutionRecursive, lessThan3) { 
+
+// 	ASSERT_EQ(6, solutionRecursive({1, 2, 6}));
+// 	ASSERT_EQ(4, solutionRecursive({1, 25}));
+// 	ASSERT_EQ(2, solutionRecursive({1}));
+// 	ASSERT_EQ(0, solutionRecursive({}));
+// }
+
+// TEST(solutionRecursive, greaterThan24) { 
+
+// 	ASSERT_EQ(25, solutionRecursive({1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}));
+// 	ASSERT_EQ(25, solutionRecursive({4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,28, 29,30}));
+// }
+
+
+
+TEST(solutionRecursive, HKtest) { 
+
+	ASSERT_EQ(7, solutionRecursive({1, 2, 4, 5	}));
+	// ASSERT_EQ(7, solutionRecursive({5,6,7,8,9,10}));
+
+	// ASSERT_EQ(14, solutionRecursive({5,6,7,8,9,10, 12,13,14,15}));
+
+	// ASSERT_EQ(16, solutionRecursive({5,6,7,8,9,10, 21,27,28,29,30}));
+}
+
 /*
 TEST(InitalValueTest, checkRandNum) { 
 
