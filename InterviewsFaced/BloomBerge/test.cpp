4c4a4d6f78376f5bc03c1f59acbe6f5414717aee
ranadewa <ranadewa.thisara@gmail.com>
2018-04-21 11:56:54 +0700

    Initial work

---
+++ InterviewsFaced/BloomBerge/test.cpp
@@ -0,0 +1,15 @@
+#include "Main.cpp"
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+
+/*
+TEST(InitalValueTest, checkRandNum) { 
+sss	
+		ASSERT_EQ('Z', testMap[rand()]);
+}
+*/
